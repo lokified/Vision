5f06103e254bced21d0c459da94515ad61773c39
lokified <lsheldon645@gmail.com>
2023-07-12 19:08:18 +0300

    add crashlytics and spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,37 @@ plugins {
     id("com.google.gms.google-services") version "4.3.15" apply false
     id("com.google.dagger.hilt.android") version "2.45" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+    id("com.google.firebase.crashlytics") version "2.9.6" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("com.diffplug.spotless") version "6.19.0"
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    ktlint {
+        android.set(true)
+        verbose.set(true)
+        filter {
+            exclude { element -> element.file.path.contains("generated/") }
+        }
+    }
+}
+
+subprojects {
+
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${project.rootDir}/build-logic/**/*.kt")
+        }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+        }
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+        }
+    }
 }
